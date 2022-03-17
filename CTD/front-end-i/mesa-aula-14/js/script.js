f3f064a157a3e5a38404b432ea8f520018ef4c24
Cauê Silveira <99031403+silveirac@users.noreply.github.com>
2022-03-17 20:44:40 -0300

    delete folder mesa-aula-14

--- CTD/front-end-i/mesa-aula-14/js/script.js
+++
@@ -1,8 +0,0 @@
-function togleMobileMenu(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
-}
