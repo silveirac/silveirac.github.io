a887f2ed1274d10a748b273fa24fd0a3bf0ec4e0
silveirac <caueasilveira@gmail.com>
2022-05-18 20:02:56 -0300

    archive update

--- CTD/programacao-imperativa/checkpoint-1/checkpoint-1.js
+++ CTD/programacao-imperativa/checkpoint-1/checkpoint-1.js
@@ -29,13 +29,13 @@ function microondas (opcao, tempo) {
 
     // Checando o tempo em relação ao tempo padrão do prato escolhido
     if (tempo < tempoPadrao) {
-        return "Tempo insuficiente." + " Prato pronto, bom apetite!!!";
+        return "Tempo insuficiente. Prato pronto, bom apetite!!!";
     } else  if (tempo > (2 * tempoPadrao) && tempo <= (3 * tempoPadrao)) {
-        return "A comida queimou." + " Prato pronto, bom apetite!!!";
+        return "A comida queimou. Prato pronto, bom apetite!!!";
     } else if (tempo > (3 * tempoPadrao)) {
-        return "Kabum!" + " Prato pronto, bom apetite!!!";
+        return "Kabum! Prato pronto, bom apetite!!!";
     } else {
-        return " Prato pronto, bom apetite!!!"
+        return "Prato pronto, bom apetite!!!"
     }
 
 }
