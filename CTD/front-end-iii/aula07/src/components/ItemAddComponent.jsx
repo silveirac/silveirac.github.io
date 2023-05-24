369b5e646c9caee9b396c212b536b2b2bf147b29
silverirac <caueasilveira@gmail.com>
2023-05-23 22:38:44 -0300

    minor alterations

--- CTD/front-end-iii/aula07/src/components/ItemAddComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function ItemAddComponent ({array}) {
-    return (
-        <div>
-            <input type="text" name="text-field" id="text-field" value={description} />
-            <input type="button" onClick={() => setItems(array.push ())} />
-        </div>
-    ) 
-}
